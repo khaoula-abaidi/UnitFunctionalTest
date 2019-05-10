a9f5c11d15d8b39e298caf2fe5b2accbf0feb405
khaoula-abaidi <abaidik@gmail.com>
2019-05-10 16:46:53 +0200

    Simple Translation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(TranslatorInterface $translator)
     {
         $me = 'mes salutations test';
+        $translated = $translator->trans('Symfony is great');
         return $this->render('home/index.html.twig', [
             'me' => $me,
+            'translated' => $translated
         ]);
     }
 }
